c2b5a05556f555c7af09dcba854de410245803ef
amvlasenko <61311215+amvlasenko@users.noreply.github.com>
2021-02-03 06:52:37 +0300

    Промежуточный вариант
    Почти готова мобильная версия главной страницы

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,33 @@ const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const webp = require("gulp-webp");
+const svgstore = require('gulp-svgstore');
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+
+// Imgmin
+
+const imgmin = () => {
+  gulp.src("testimg/*.jpg")
+        .pipe(webp({quality: 50}))
+        .pipe(gulp.dest("build/img"))
+}
+
+exports.imgmin = imgmin;
+
+// SVG sprite
+
+const sprite = () => {
+  gulp.src("source/gram/*.svg")
+        .pipe(svgstore())
+        .pipe(rename("icons-sprite.svg"))
+        .pipe(gulp.dest("build/img"))
+}
+
+exports.sprite = sprite;
 
 // Styles
 
